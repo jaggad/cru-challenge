d1188e422c89a5772eb691f0d6e9932d5a4602df
Jack Edgson <jack@jackedgson.com>
2021-12-18 22:33:00 +1000

    chore(test): get tests running

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,11 @@ module.exports = {
       https://jestjs.io/docs/webpack#handling-static-assets */
     '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$':
       '<rootDir>/__mocks__/fileMock.js',
+
+    '@lib/(.*)': '<rootDir>/src/lib/$1',
+    '@pages/(.*)': '<rootDir>/src/pages/$1',
+    '@components/(.*)': '<rootDir>/src/components/$1',
+    '@containers/(.*)': '<rootDir>/src/containers/$1',
   },
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
